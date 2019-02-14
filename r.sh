ffa0fccd986ca04a9e99621ef4774a4e52fd69ce
InYourG00D1 <44290386+InYourG00D1@users.noreply.github.com>
2019-02-14 22:35:29 +0700

    Add files via upload

---
+++ r.sh
@@ -0,0 +1,307 @@
+#!/system/xbin/bash
+#Jangan di recode dong ster 
+#usaha kalo gk bisa nanya 
+
+
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+echo "\033[32;1m]======================================================="
+echo "\033[31;1m] ^...^  Tools InstaPler"
+echo "\033[31;1m]<_* *_> Coded by : InYourG00D"
+echo "\033[31;1m]  \_/   Team : LNX#CREW S.T.C BUFT"
+echo "\033[36;1m] kontak Me : 6281382406878"
+echo "\033[33;1m] Git Hub https://github.com/InYourG00D1"
+echo "\033[32;1m]======================================================="
+echo $red "[!]MENU"
+echo $green "[===============================[>"
+echo "1.PHISING"|lolcat
+echo $cyan "[===============================[>"
+echo "2.RED HAWK"|lolcat
+echo $blue "[===============================[>"
+echo "3.Bikin Script Deface"|lolcat
+echo $purple "[===============================[>"
+echo "4.Install DDOS"
+echo $yellow "[===============================[>"
+echo "5.Install WaterDos"|lolcat
+echo $red "[===============================[>"
+echo "6.Install Xerxes"
+echo $green "[===============================[>"
+echo "7.Install Spam Bom Tree"
+echo $white "[===============================[>"
+echo "8.Install LiteSpam"|lolcat
+echo $red "[===============================[>"
+echo "9.Install Lazymux"|lolcat
+echo $cyan "[===============================[>"
+echo "10.Install VlunScan"
+echo $purple "[===============================[>"
+echo "11.Lacak Ip"|lolcat
+echo $blue "[===============================[>"
+echo "12.CheckBin"|lolcat
+echo $red "[===============================[>"
+echo "13.Install FB brute"|lolcat
+echo $blue "[===============================[>"
+echo "14.Animasi Kereta"|lolcat
+echo $green "[===============================[>"
+echo "15.Install Hack IG"|lolcat
+echo $purle "[===============================[>"
+echo "16.Install Weeman"|lolcat
+echo $white "[===============================[>"
+echo "17.Install OSIF"|lolcat
+echo $yellow "[===============================[>"
+echo "18.Install LazySqlmap"|lolcat
+echo $blue "[===============================[>"
+echo "19.Install Wpscan"|lolcat
+echo $green "[===============================[>"
+echo "20.Install NetKiller"|lolcat
+echo $white "[===============================[>"
+echo "21.Install Credmap"|lolcat
+echo $yellow "[===============================[>"
+echo "22.Install TermuxSudo"|lolcat
+echo $cyan "[===============================[>"
+echo "23.Install 1337Hash"|lolcat
+echo $blue "[===============================[>"
+echo "24.Install BitcoinWallet"|lolcat
+echo $red "[===============================[>"
+echo "25.Install BruteForceGmail"|lolcat
+echo $green "[===============================[>"
+echo "26.Install Nuyul Bitcoin"|lolcat
+echo $cyan "[===============================[>"
+echo "27.Install BlackHydra"|lolcat
+echo $blue "[===============================[>"
+echo "28.Informasi Android"|lolcat
+echo $white "[===============================[>"
+echo "29.Install Striker"|lolcat
+echo $purple "[===============================[>"
+echo "30.Install SANTET ONLINE"|lolcat
+echo $blue "[===============================[>"
+echo "00.Out"|lolcat
+read -p "[root@IndoDarkHeart~#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+pkg install python
+pkg instal git
+git clone https://github.com/UndeadSec/SocialFish.git
+echo "done"|lolcat
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then 
+clear 
+git clone https://github.com/4L13199/LITESCRIPT
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then 
+clear 
+git clone https://github.com/4L13199/LITEDDOS
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 5 ] || [ $bro = 5]
+then 
+clear
+git clone https://github.com/JeyZeta/WaterDoS
+echo "-======DONE======-"
+fi 
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then 
+clear
+git clone https://github.com/zanyarjamal/xerxes
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then 
+clear
+git clone https://github.com/Gameye98/SpazSMS
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then 
+clear
+git clone https://github.com/4L13199/LITESPAM
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then 
+clear
+git clone https://github.com/Gameye98/Lazymux.git
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then 
+clear
+git clone https://github.com/kereh/VulnScaner
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then 
+clear
+git clone https://github.com/InYourG00D1/LacakIP.N2
+cd LacakIP.N2
+python2 LacakIP#.py
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then 
+clear
+https://github.com/InYourG00D1/check
+cd check
+php bin.php
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then 
+clear
+git clone https://github.com/mrmsdv/fbbrute.git
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then 
+clear
+pkg install sl
+sl
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then 
+clear
+git clone https://github.com/avramit/instahack.git
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 16 ] || [ $bro = 16 ]
+then 
+clear
+git clone https://github.com/evait-security/weeman.git
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 17 ] || [ $bro = 17 ]
+then 
+clear
+git clone https://github.com/CiKu370/OSIF.git
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 18 ] || [ $bro = 18 ]
+then 
+clear
+git clone https://github.com/verluchie/termux-lazysqlmap
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 19 ] || [ $bro = 19 ]
+then 
+clear
+git clone https://github.com/wpscanteam/wpscan
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 20 ] || [ $bro = 20 ]
+then 
+clear
+git clone https://github.com/sysadmimteam/NetKiller
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 21 ] || [ $bro = 21 ]
+then 
+clear
+git clone https://github.com/lightos/credmap
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 22 ] || [ $bro = 22 ]
+then 
+clear
+git clone https://github.com/st42/termux-sudo
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 23 ] || [ $bro = 23 ]
+then 
+clear
+git clone https://github.com/Gameye98/1337Hash
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 24 ] || [ $bro = 24 ]
+then 
+clear
+git clone https://github.com/bitcoin/bitcoin-wallet
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 25 ] || [ $bro = 25 ]
+then 
+clear
+git clone https://github.com/JamesAndresCM/Brute_force_gmail
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 26 ] || [ $bro = 26 ]
+then 
+clear
+git clone https://github.com/Cvar1984/pemulungBTC.git
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 27 ] || [ $bro = 27 ]
+then 
+clear
+git clone https://github.com/Gameye98/Black-Hydra.git
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 28 ] || [ $bro = 28 ]
+then 
+clear
+pkg install neofetch
+neofetch
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 29 ] || [ $bro = 29 ]
+then 
+clear
+git clone https://github.com/UltimateHackers/Striker
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 30 ] || [ $bro = 30 ]
+then 
+clear
+git clone https://github.com/Gameye98/santet-online
+echo "-=====DONE=====-"|lolcat
+fi
+
+if [ $bro = 00 ] || [ $bro = 00 ]
+then 
+clear
+echo "thankYou"
+echo "SeeYouAgain"
+exit
+fi
